fc0a9c1461cc5469eece2f2dd3d59964682a4676
Antonio <antoniogagos@gmail.com>
2017-03-09 17:37:15 +0100

    changes

--- project.py
+++ project.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template, request, redirect, jsonify, \
     url_for, flash
 from sqlalchemy import create_engine, asc
@@ -15,8 +17,7 @@ import requests
 
 app = Flask(__name__)
 
-CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web'
-        ]['client_id']
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = 'pc-components-catalog'
 
 # Connect to Database and create database session
@@ -27,10 +28,13 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 # Static pages
 @app.route('/')
 @app.route('/components')
 def showComponents():
+    """ Main page show component list """
+
     username = login_session.get('username')
     picture = login_session.get('picture')
     user_email = login_session.get('email')
@@ -45,6 +49,8 @@ def showComponents():
 
 @app.route('/components/<string:component>/')
 def showComponentItems(component):
+    """ Show items inside a component """
+
     component = session.query(Component).filter_by(name=component).one()
     allItems = session.query(Item).filter_by(component=component).all()
     username = login_session.get('username')
@@ -81,31 +87,36 @@ def showItem(component, item):
 
 @app.route('/components/add', methods=['GET', 'POST'])
 def addItem():
-    if request.method == 'POST':
-        component_name = request.form['component']
-        component = \
-            session.query(Component).filter_by(name=component_name).one()
-        user_email = login_session.get('email')
+    username = login_session.get('username')
 
-        price_item = str(request.form['price'])
-        newItem = Item(name=request.form['name'], price=price_item,
-                       description=request.form['description'],
-                       component=component, email=user_email)
-        session.add(newItem)
-        session.commit()
+    if username:
+        if request.method == 'POST':
+            component_name = request.form['component']
+            component = \
+                session.query(Component).filter_by(name=component_name).one()
+            user_email = login_session.get('email')
 
-        return redirect(url_for('showComponentItems',
-                        component=request.form['component']))
+            price_item = str(request.form['price'])
+            newItem = Item(name=request.form['name'], price=price_item,
+                           description=request.form['description'],
+                           component=component, email=user_email)
+            session.add(newItem)
+            session.commit()
+
+            return redirect(url_for('showComponentItems',
+                            component=request.form['component']))
+        else:
+            username = login_session.get('username')
+            picture = login_session.get('picture')
+            user_email = login_session.get('email')
+
+            state = ''.join(random.choice(string.ascii_uppercase
+                            + string.digits) for x in xrange(32))
+            login_session['state'] = state
+            return render_template('add-item.html', username=username,
+                                   picture=picture, STATE=state)
     else:
-        username = login_session.get('username')
-        picture = login_session.get('picture')
-        user_email = login_session.get('email')
-
-        state = ''.join(random.choice(string.ascii_uppercase
-                        + string.digits) for x in xrange(32))
-        login_session['state'] = state
-        return render_template('add-item.html', username=username,
-                               picture=picture, STATE=state)
+        return redirect(url_for('showComponents'))
 
 
 @app.route('/components/<string:component>/<string:item>/edit',
@@ -157,6 +168,8 @@ def editItem(component, item):
 @app.route('/components/<string:component>/<string:item>/delete',
            methods=['GET', 'POST'])
 def deleteItem(component, item):
+    """ Delete method for a specific item """
+
     user_email = login_session.get('email')
     item = session.query(Item).filter_by(name=item).one()
     if user_email == item.email:
@@ -184,12 +197,15 @@ def deleteItem(component, item):
 
 @app.route('/components/JSON')
 def componentsJSON():
+    """ Returns JSON version of the components """
+
     components = session.query(Component).all()
     return jsonify(components=[c.serialize for c in components])
 
 
 @app.route('/components/<string:component>/JSON')
 def componentItemsJSON(component):
+    """ Returns JSON version of the items inside a component """
     component = session.query(Component).filter_by(name=component).one()
     items = session.query(Item).filter_by(component=component).all()
     print items
@@ -198,46 +214,41 @@ def componentItemsJSON(component):
 
 @app.route('/components/<string:component>/<string:item>/JSON')
 def itemJSON(component, item):
+    """ Returns JSON version of a specific item """
+
     item = session.query(Item).filter_by(name=item).one()
     return jsonify(item=item.serialize)
 
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    """ Login with google method. Return error if it's occurred or
+        success output """
 
     # Validate state token
-
     if request.args.get('state') != login_session['state']:
-        response = make_response(json.dumps('Invalid state parameter.'
-                                 ), 401)
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
 
     # Obtain authorization code
-
     code = request.data
 
     try:
-
         # Upgrade the authorization code into a credentials object
 
-        oauth_flow = flow_from_clientsecrets('client_secrets.json',
-                scope='')
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
-        response = \
-            make_response(json.dumps('Failed to upgrade the authorization code.'
-                          ), 401)
+        response = make_response(json.dumps('Failed auth code.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
 
     # Check that the access token is valid.
 
     access_token = credentials.access_token
-    url = \
-        'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' \
-        % access_token
+    url = 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
 
@@ -252,9 +263,8 @@ def gconnect():
 
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
-        response = \
-            make_response(json.dumps("Token's user ID doesn't match given user ID."
-                          ), 401)
+        response = make_response(json.dumps("Token doesn't match user."), 401)
+
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -262,8 +272,7 @@ def gconnect():
 
     if result['issued_to'] != CLIENT_ID:
         response = \
-            make_response(json.dumps("Token's client ID does not match app's."
-                          ), 401)
+            make_response(json.dumps("Token client doesn't match."), 401)
         print "Token's client ID does not match app's."
         response.headers['Content-Type'] = 'application/json'
         return response
@@ -271,9 +280,7 @@ def gconnect():
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = \
-            make_response(json.dumps('Current user is already connected.'
-                          ), 200)
+        response = make_response(json.dumps('User connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -305,20 +312,18 @@ def gconnect():
 
 @app.route('/gdisconnect')
 def gdisconnect():
-
+    """ Disconnect method deleting user login_session dictionary"""
     # Only disconnect a connect user
 
     credentials = login_session.get('credentials')
     if credentials is None:
-        response = make_response(json.dumps('Current user not connected'
-                                 ), 401)
+        response = make_response(json.dumps('Current user not connected'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
 
     # Execute HTTP GET request to revoke current token.
 
-    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' \
-        % credentials
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % credentials
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
 
@@ -332,17 +337,14 @@ def gdisconnect():
         del login_session['email']
         del login_session['picture']
 
-        response = make_response(json.dumps('Successfully disconnected.'
-                                 ), 200)
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return redirect(url_for('showComponents'))
     else:
 
         # For whatever reason, the given token was invalid.
 
-        response = \
-            make_response(json.dumps('Failed to revoke token for given user.'
-                          ), 400)
+        response = make_response(json.dumps('Failed to revoke token.'), 400)
         response.headers['Content-Type'] = 'application/json'
         return response
 
